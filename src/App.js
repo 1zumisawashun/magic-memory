ca885a32ddd64a95afbaee928537ec630c24ba58
1zumisawashun <shun.1zumisawa@gmail.com>
2021-12-03 21:47:03 +0900

    add-single-card-components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,84 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import SingleCard from "./components/SingleCard";
+
+const cardImages = [
+  { src: "/img/helmet-1.png", matched: false },
+  { src: "/img/potion-1.png", matched: false },
+  { src: "/img/ring-1.png", matched: false },
+  { src: "/img/scroll-1.png", matched: false },
+  { src: "/img/shield-1.png", matched: false },
+  { src: "/img/sword-1.png", matched: false },
+];
 
 function App() {
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+  const [choiceOne, setChoiceOne] = useState(null);
+  const [choiceTwo, setChoiceTwo] = useState(null);
+  // shuffle cards
+  const shuffleCards = () => {
+    const shuffleCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }));
+
+    setCards(shuffleCards);
+    setTurns(0);
+  };
+
+  // handle choice
+  const handleChoice = (card) => {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+    console.log(card);
+  };
+
+  //compare 2 selected cards
+  //when fire handleChoice, handle useEffect
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+      if (choiceOne.src === choiceTwo.src) {
+        setCards((prevCards) => {
+          return prevCards.map((card) => {
+            if (card.src === choiceOne.src) {
+              console.log("those cards are matched");
+              return { ...card, matched: true };
+            } else {
+              console.log("those cards do not match");
+              return card;
+            }
+          });
+        });
+        resetTrun();
+      } else {
+        console.log("those cards do not match");
+        setTimeout(() => resetTrun(), 1000);
+      }
+    }
+  }, [choiceOne, choiceTwo]);
+
+  console.log(cards);
+
+  //reset choices & increase turn
+  const resetTrun = () => {
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setTurns((prevTruns) => prevTruns + 1);
+  };
+
   return (
     <div className="App">
       <h1>Magic Match</h1>
-      <button>New Game</button>
+      <button onClick={shuffleCards}>New Game</button>
+      <div className="card-grid">
+        {cards.map((card) => (
+          <SingleCard
+            key={card.id}
+            card={card}
+            handleChoice={handleChoice}
+            flipped={card === choiceOne || card === choiceTwo || card.matched}
+          />
+        ))}
+      </div>
     </div>
   );
 }
